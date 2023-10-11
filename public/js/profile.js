05b1a100078c7eafb1cfe3745c21bb2b9c0778dd
pcspread <drummer0125vocal@sea.chukai.ne.jp>
2023-10-11 14:32:09 +0900

    creating profile

--- public/js/profile.js
+++ public/js/profile.js
@@ -10,3 +10,28 @@ function openFile() {
     });
 }
 openFile();
+
+/**
+ * 選択されたファイルを表示する
+ */
+function fileView() {
+    const input = document.querySelector('.profile-image__click');
+    const image = document.querySelector('.profile-image__instance');
+
+    input.addEventListener('change', function () {
+        const file = input.files[0];
+
+        if (file) {
+            const reader = new FileReader();
+
+            reader.onload = function (e) {
+                image.src = e.target.result;
+            }
+
+            reader.readAsDataURL(file);
+        } else {
+            image.src = 'https://dummyimage.com/100x100/000/0011ff';
+        }
+    });
+}
+fileView();
